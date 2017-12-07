7f3404f19dc8436c6bf5c788a8ac1e5e85d99df3
Giordan Battaglin <gio@giordan.ca>
2017-12-07 15:39:42 -0600

    styles and nav work

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
+import Logo from './asterisk.svg';
+
 class Header extends Component {
   render() {
     return (
       <nav>
+        <span className="img-wrap">
+          <img src={ Logo } alt=""/>
+        </span>
         <ul>
-          <li><a href="">Home</a></li>
+          <li><a href="">About Us</a></li>
           <li><a href="">Gallery</a></li>
           <li><a href="">Buy Now</a></li>
+          <li><a href="">Contact</a></li>
         </ul>
       </nav>
     );
