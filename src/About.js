c231b1b6b77b1c6c79afcfe0463c0f00bd557380
Giordan Battaglin <gio@giordan.ca>
2017-12-24 11:02:40 -0500

    added new font

--- src/About.js
+++ src/About.js
@@ -4,14 +4,23 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <h1>
-          Welcome to JadeMade!
-        </h1>
-        <h2>
-          We make amazing custom cards for any occasion.
-        </h2>
-    </div>
+      <div className="about">
+        <div className="section">
+          <div className="wrapper">
+            <h1>
+              Welcome to JadeMade!
+            </h1>
+            <h2>
+              We make amazing custom cards for any occasion.
+            </h2>
+          </div>
+        </div>
+        <div className="section">
+          <div className="wrapper">
+            <img src="/sparkles.jpg" alt=""/>
+          </div>
+        </div>
+      </div>
     );
   }
 }
